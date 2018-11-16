75a2bc73663fc5df2880a718920b1d0f7954ba84
Franck Aragão <fjunior.aragao@gmail.com>
2018-11-16 03:25:04 -0200

    ajuste server.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
@@ -26,9 +25,9 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port)
   consola.ready({
-    message: `Server listening on http://${host}:${port}`,
+    message: `Server listening`,
     badge: true
   })
 }
